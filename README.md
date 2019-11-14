fe364390070b6ab68214da8f35be37bbda857079
BCButcher <b.butcher@mail.utoronto.ca>
2019-11-14 09:11:59 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-u9ap9d-2rfflb
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-u9ap9d-2rfflb)
---
+++ src/app/product-alerts/product-alerts.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { Input } from '@angular/core';
+import { Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-product-alerts',
+  templateUrl: './product-alerts.component.html',
+  styleUrls: ['./product-alerts.component.css']
+})
+export class ProductAlertsComponent implements OnInit {
+  @Input() product;
+  @Output() notify = new EventEmitter();
+  }
+
+export class ProductListComponent {
+  products  = products;
+
+  share() {
+    window.alert('The product has been shared!');
+  }
+}
